786a87401ad51b366b89656abbc7f829c38e6929
SpringBanana <danilaost2@gmail.com>
2020-02-19 19:48:04 +0300

    ``

--- SumDouble/SumDouble.java
+++ SumDouble/SumDouble.java
@@ -8,15 +8,15 @@ public class SumDouble {
         int pointer = 0;
         for (int i = 0; i < args.length; i++) {
             pointer = 0;
-            for(int j = 0; j <= args[i].length(); j++) {
-                if (j == args[i].length() || Character.isWhitespace(args[i].charAt(j))) {
-                    if (pointer > 0) {
-                        result += Double.parseDouble(args[i].substring(j - pointer, j));
+                for(int j = 0; j <= args[i].length(); j++) {
+                    if (j == args[i].length() || Character.isWhitespace(args[i].charAt(j))) {
+                        if (pointer > 0) {
+                            result += Double.parseDouble(args[i].substring(j - pointer, j));
+                        }
+                        pointer = 0;
+                    } else {
+                        pointer++;
                     }
-                    pointer = 0;
-                } else {
-                    pointer++;
-                }
             }
         }
         System.out.println(result);
